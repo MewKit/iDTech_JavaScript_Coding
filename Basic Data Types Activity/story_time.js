219c4635a3074ac35df0115358e8ce0154616795
MewKit <selena.owl129@gmail.com>
2022-07-07 10:21:01 -0700

    basic data type

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,28 @@
+let intro = "up in the sky flew the legendary owl";
+let name = "Jimmy";
+let title = "Fierce Watermelon";
+let opponent = "Fuzzy the Pure Sugar";
+
+let age = 101;
+let year = 1901;
+let century = 1;
+
+let items = ['bombs', 'sugar', 'watermelon seeds', 'dog'];
+
+let isaWizard = true;
+
+console.log("Once upon a time, " + intro + " called " + name + ".");
+console.log("He was known as the " + title + " who had a ongoing feud with " + opponent + ".");
+console.log("It all started in the " + 1 + "st century in the year " + year + ".");
+console.log("At that time, " + name + " was only " + age + " years old" + ".");
+console.log("He was looking for some " + items[2] + " when " + opponent + " happened to be looking for some " + items[1] + " at the same store.");
+console.log("They recognized each other from a flying contest last year, where they were both announced the winner for crossing the finish line at the same time.");
+console.log("Within three seconds, the store was covered with " + items[2] + " and " + items[1] + ".");
+console.log("They were both exhausted from the so-called dodgeball tournament, but " + name + " had one final strike to make.");
+console.log("He got out a stick with an orb on it and whooshed it around.");
+console.log("A pile of " + items[0] + " appeared along with a dog.");
+console.log("It was indeed " + isaWizard + ".");
+console.log(name + " was a wizard.")
+console.log("He rode on the " + items[3] +", held onto his " + items[0] + ", and charged up to " + opponent + ".");
+console.log("It was realized that the " + items[0] +" actually contained " + items[1] + ", so " + opponent + " just devoured it.");
+console.log("To this day, their ongoing feud is about whether or not it was " + name + "'s fault for " + opponent + " to continuously be on sugar high.")
